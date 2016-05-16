863fc7430765d7dea73c7631e4516e069e43042e
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-05-15 20:28:42 -0400

    divs adding

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,10 +14,11 @@ $(document).ready(function () {
 		firstTime= $('.addFirst').val().trim();
 		duration= $('.addDuration').val().trim();
 
-		console.log(trainname);
-		console.log(destination);
-		console.log(firstTime);
-		console.log(duration);
+
+		// console.log(trainname);
+		// console.log(destination);
+		// console.log(firstTime);
+		// console.log(duration);
 
 		inputInfo.push({
 			Name: trainname,
@@ -26,8 +27,43 @@ $(document).ready(function () {
 			Duration: duration,
 			dateadded: Firebase.ServerValue.TIMESTAMP,
 		})
+
+		return false;
+	});
+
+	inputInfo.on("child_added", function(childSnapshot) {
+
+
+
+		console.log(childSnapshot.val().Name);
+		console.log(childSnapshot.val().Destination);
+		console.log(childSnapshot.val().FirstTrainTime);
+		console.log(childSnapshot.val().Duration);
+
+		$('.trainNameFill').append("<div class='well'>"+
+			childSnapshot.val().Name+"</div>");
+		$('.DestinationFill').append("<div class='well'>"+
+			childSnapshot.val().Destination+"</div>");
+		$('.FreqFill').append("<div class='well'>"+
+			childSnapshot.val().FirstTrainTime+"</div>");
+		$('.NextFill').append("<div class='well'>"+
+			childSnapshot.val().Duration+"</div>");
+
+		
+
+
+		}, function(errorObject){
+		console.log("Errors handled: " + errorObject.code);
 	});
 
 
 
+
+
 });
+
+
+	// <span id='email'> "+
+	// 		childSnapshot.val().email+" </span><span id='age'> "+
+	// 		childSnapshot.val().age+" </span><span id='comment'> "+
+	// 		childSnapshot.val().comment+" </span></div>")
